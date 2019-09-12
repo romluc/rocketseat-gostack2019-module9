5f9f56b69df1bcfefa7f5c02a3201a1b4b4c7a73
romluc <romulofilhofilho@gmail.com>
2019-09-12 15:22:36 -0300

    Create pages structure and add Reactotron

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+import Routes from './routes';
+import history from './services/history';
+
+import './config/ReactotronConfig';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <Router history={history}>
+      <Routes />
+    </Router>
+  );
 }
 
 export default App;
