85dcdd3818e06feb044a9811cfa6693b59c665c3
romluc <romulofilhofilho@gmail.com>
2019-09-12 16:34:06 -0300

    Create initial layout models

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-js';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect();
+  const tron = Reactotron.configure().connect({ host: '192.168.15.11' });
 
   tron.clear();
 
