bcb3c30e5135d6c7b13412aa52ccac431b82583f
romluc <romulofilhofilho@gmail.com>
2019-09-15 18:36:35 -0300

    Add react-toastify

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -17,7 +18,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('User is not a provider');
+      toast.error('User is not a provider');
       return;
     }
 
@@ -25,6 +26,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Authentication failure. Please verify your data.');
     yield put(signFailure());
   }
 }
